6ba6bac7ae0f550a31c52014531a966fbe29d19a
Peter Schmidt <schmidt.multimedia@gmail.com>
2023-04-24 11:54:03 +0200

    Add files via upload

---
+++ com/cc/java/Parent.java
@@ -0,0 +1,16 @@
+package com.cc.java;
+
+public class Parent extends GrandParent {
+    
+
+    // public String sayHello() {
+    //     return "Hello from Parent!";
+    // }
+
+    public String sayHello(String name) {
+        return "Hello " + name +"!";
+    }
+
+
+
+}
